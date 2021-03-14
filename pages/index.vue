89fb3ca9d02f48c040b1a3411dc156d09cd9c98c
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-15 01:59:49 +0900

    timer実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="flex h-screen w-screen bg-gray-200">
-    <div class="m-auto">{{ value }}</div>
+  <div class="flex h-screen w-screen bg-gray-200" @click="touch()">
+    <div class="m-auto text-6xl">{{ (elapsedTime / 1000).toFixed(3) }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+const STATE = {
+  wait: 'wait',
+  running: 'running',
+  stop: 'stop',
+} as const
+
+type State = typeof STATE[keyof typeof STATE]
+
+type Data = {
+  state: State
+  startTime: number
+  stopTime: number
+  elapsedTime: number
+  timerID: number
+}
+
 export default Vue.extend({
   name: 'App',
-  data() {
+  data(): Data {
     return {
-      value: 'hello world',
+      state: STATE.wait,
+      startTime: 0,
+      stopTime: 0,
+      elapsedTime: 0,
+      timerID: 0,
     }
+  },
+  methods: {
+    touch() {
+      switch (this.state) {
+        case STATE.wait:
+          this.startTime = Date.now()
+          this.timerID = window.setInterval(() => {
+            this.elapsedTime = Date.now() - this.startTime
+          }, 10)
+          this.state = STATE.running
+          break
+        case STATE.running:
+          this.stopTime = Date.now()
+          clearInterval(this.timerID)
+          this.elapsedTime = this.stopTime - this.startTime
+          this.state = STATE.stop
+          break
+        case STATE.stop:
+          this.elapsedTime = 0
+          this.state = STATE.wait
+          break
+      }
+    },
   },
 })
 </script>
