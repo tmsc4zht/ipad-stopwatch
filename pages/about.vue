9dc7f7ec10b09e7cec62a89305bd99b42aa09898
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-15 21:37:32 +0900

    add linktoabout

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex h-screen w-screen bg-gray-200 relative">
+    <nuxt-link to="/">
+      <svg
+        class="h-10 w-10 absolute top-0 right-0 mt-2 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
